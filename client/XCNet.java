7a4e2f3d526f37f6778471ac0caa84603442795a
W.M. <Wingman4l7@gmail.com>
2013-07-19 13:41:45 +1000

    added missing license headers
    It was stated that this programmer "created a networked prototype of
    Flight Club" but I do not know for sure the extent of his contribution.

--- client/XCNet.java
+++ client/XCNet.java
@@ -1,3 +1,16 @@
+/*
+  @(#)XCNet.java (part of 'Flight Club')
+  
+  Includes contributions from Artem Nikulchev (jaguart@paragliding.ru), 
+  a paraglider pilot from Moscow.  Presumably, the below still applies:
+  
+  This code is covered by the GNU General Public License
+  detailed at http://www.gnu.org/copyleft/gpl.html
+	
+  Flight Club docs located at http://www.danb.dircon.co.uk/hg/hg.htm
+  Copyright 2001-2003 Dan Burton <danb@dircon.co.uk>
+*/
+
 package flightclub.client;
 
 import java.io.*;
--- server/XCGameServer.java
+++ server/XCGameServer.java
@@ -1,3 +1,16 @@
+/*
+  @(#)XCGameServer.java (part of 'Flight Club')
+  
+  Includes contributions from by Artem Nikulchev (jaguart@paragliding.ru), 
+  a paraglider pilot from Moscow.  Presumably, the below still applies:
+  
+  This code is covered by the GNU General Public License
+  detailed at http://www.gnu.org/copyleft/gpl.html
+	
+  Flight Club docs located at http://www.danb.dircon.co.uk/hg/hg.htm
+  Copyright 2001-2003 Dan Burton <danb@dircon.co.uk>
+*/
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
