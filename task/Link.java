cd3c27626f548cb01077bff2ece7baea9b28e810
W.M. <Wingman4l7@gmail.com>
2013-07-19 12:26:07 +1000

    added missing license / info header to class

--- task/Link.java
+++ task/Link.java
@@ -1,3 +1,13 @@
+/*
+  @(#)Link.java (part of 'Flight Club')
+	
+  This code is covered by the GNU General Public License
+  detailed at http://www.gnu.org/copyleft/gpl.html
+	
+  Flight Club docs located at http://www.danb.dircon.co.uk/hg/hg.htm
+  Copyright 2001-2003 Dan Burton <danb@dircon.co.uk>
+*/
+
 package flightclub.task;
 
 import java.awt.*;
