7bd64e66478259584d34d7b251def8f515338c72
W.M. <Wingman4l7@gmail.com>
2013-07-19 12:28:35 +1000

    added missing license header, fixed indentation

--- task/FileFormatException.java
+++ task/FileFormatException.java
@@ -1,9 +1,22 @@
-/** This exception may be thrown by parsers. */
+/*
+  @(#)FileFormatException.java (part of 'Flight Club')
+	
+  This code is covered by the GNU General Public License
+  detailed at http://www.gnu.org/copyleft/gpl.html
+	
+  Flight Club docs located at http://www.danb.dircon.co.uk/hg/hg.htm
+  Copyright 2001-2003 Dan Burton <danb@dircon.co.uk>
+*/
 
 package flightclub.task;
 
 import java.io.*;
 
 public class FileFormatException extends IOException {
-    public FileFormatException(String s) { super(s); }
+	/** 
+		This exception may be thrown by parsers. 
+	*/
+    public FileFormatException(String s) { 
+		super(s); 
+	}
 }
