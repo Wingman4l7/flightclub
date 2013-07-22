bb91a2108e21c9ccdaf02fea8d70b75d05c3cde2
W.M. <Wingman4l7@gmail.com>
2013-07-22 15:11:12 +1000

    fixed indents, added missing license header

--- client/XCModelCanvas.java
+++ client/XCModelCanvas.java
@@ -1,3 +1,13 @@
+/*
+ * @(#)XCModelCanvas.java (part of 'Flight Club')
+ * 
+ * This code is covered by the GNU General Public License
+ * detailed at http://www.gnu.org/copyleft/gpl.html
+ *	
+ * Flight Club docs located at http://www.danb.dircon.co.uk/hg/hg.htm
+ * Copyright 2001-2003 Dan Burton <danb@dircon.co.uk>
+ */
+
 package flightclub.client;
 
 import flightclub.framework3d.*;
@@ -5,16 +15,16 @@ import flightclub.framework3d.*;
 class XCModelCanvas extends ModelCanvas {
     XCModelViewer xcModelViewer;
     public XCModelCanvas(XCModelViewer xcModelViewer) {
-	super(xcModelViewer);
-	this.xcModelViewer = xcModelViewer;
+		super(xcModelViewer);
+		this.xcModelViewer = xcModelViewer;
     }
 
     protected void paintModel() {
-	super.paintModel();
-	XCModel m = xcModelViewer.xcModel;
-	if (m.mode == XCModel.USER) {
-            m.compass.draw(graphicsBuffer);
-	    m.slider.draw(graphicsBuffer);
-        }
+		super.paintModel();
+		XCModel m = xcModelViewer.xcModel;
+		if (m.mode == XCModel.USER) {
+				m.compass.draw(graphicsBuffer);
+				m.slider.draw(graphicsBuffer);
+			}
     }
 }
