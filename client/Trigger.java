37b725323ee78972525b8345fca82a323d4b537d
W.M. <Wingman4l7@gmail.com>
2013-07-22 16:30:34 +1000

    normalized indentation

--- client/Trigger.java
+++ client/Trigger.java
@@ -13,13 +13,14 @@ import flightclub.framework3d.*;
 import java.awt.Color;
 import java.io.*;
 import java.util.Random;
+
 /**
    This class implements a thermal trigger. Every cycle the trigger
-   releases a bubble of warm air that floats up to produce a
-   cloud. Well, that is the impression we hopefully evoke.
+   releases a bubble of warm air that floats up to produce a cloud.
+   Well, that is the impression we hopefully evoke.
 
-   The trigger model contains lots of dubious assumptions and gross
-   simplifacations. Good !
+   The trigger model contains lots of dubious assumptions 
+   and gross simplifications. Good!
 
    We add a veneer of noise to the otherwise clockwork like behaviour
    of the trigger without departing from the deterministic model.
@@ -53,47 +54,47 @@ public class Trigger implements ClockObserver, CameraSubject {
        have yet to evaporate) !
     */
     public Trigger(XCModelViewer xcModelViewer, StreamTokenizer st) throws IOException {
-	this.xcModelViewer = xcModelViewer;
+		this.xcModelViewer = xcModelViewer;
 
-	// parse data
-	st.nextToken();
-	x = (float) st.nval;
-	st.nextToken();
-	y = (float) st.nval;
-	st.nextToken();
-	thermalStrength = (float) st.nval;
-	st.nextToken();
-	cycleLength = (float) st.nval;
-	st.nextToken();
-	duration = (float) st.nval;
-	st.nextToken(); // gobble new line
+		// parse data
+		st.nextToken();
+		x = (float) st.nval;
+		st.nextToken();
+		y = (float) st.nval;
+		st.nextToken();
+		thermalStrength = (float) st.nval;
+		st.nextToken();
+		cycleLength = (float) st.nval;
+		st.nextToken();
+		duration = (float) st.nval;
+		st.nextToken(); // gobble new line
 
-	/**
-	   ~Random phase. We ensure that all clients get same phase
-	   for a trigger. Note that *all* triggers get created from
-	   the task file before the model comes alive - so using the
-	   pseudo rnd generator is ok.
-	*/
-	phase = random.nextFloat() * cycleLength;
-	myID = nextID++; // unique id (for debugging)
+		/*
+		   ~Random phase. We ensure that all clients get same phase
+		   for a trigger. Note that *all* triggers get created from
+		   the task file before the model comes alive - so using the
+		   pseudo rnd generator is ok.
+		*/
+		phase = random.nextFloat() * cycleLength;
+		myID = nextID++; // unique id (for debugging)
     }
 
     /** Creates a 'default' trigger at (x, y). */
     public Trigger(XCModelViewer xcModelViewer, float x, float y) {
-	this.xcModelViewer = xcModelViewer;
-	this.x = x;
-	this.y = y;
+		this.xcModelViewer = xcModelViewer;
+		this.x = x;
+		this.y = y;
 
-	// use a gaussian distribution for area (= size * size) with mean=4 and sd=2 
-	float a = 4.0f + (float) random.nextGaussian() * 2.0f; 
-	if (a < 0.5f) {
-	    a = 0.5f;
-	}
-	thermalStrength = (float) Math.sqrt(a);
-	cycleLength = Cloud.getLifeSpan(thermalStrength) * 1.0f;
-	duration = 1.0f;
-	phase = random.nextFloat() * cycleLength;
-	myID = nextID++;
+		// use a gaussian distribution for area (= size * size) with mean=4 and sd=2 
+		float a = 4.0f + (float) random.nextGaussian() * 2.0f; 
+		if (a < 0.5f) {
+			a = 0.5f;
+		}
+		thermalStrength = (float) Math.sqrt(a);
+		cycleLength = Cloud.getLifeSpan(thermalStrength) * 1.0f;
+		duration = 1.0f;
+		phase = random.nextFloat() * cycleLength;
+		myID = nextID++;
     }
 
     /**
@@ -101,55 +102,55 @@ public class Trigger implements ClockObserver, CameraSubject {
        yet to evaporate). We loop back thru' the cycles from t_.
      */
     private void existingClouds(float t) {
-	float t__ = t_ - cycleLength;
-	float cloudLifeSpan = Cloud.getLifeSpan(thermalStrength);
-	while (t - t__ < cloudLifeSpan) {
-	    makeCloud(t - t__);
-	    t__ -= cycleLength;
-	}
+		float t__ = t_ - cycleLength;
+		float cloudLifeSpan = Cloud.getLifeSpan(thermalStrength);
+		while (t - t__ < cloudLifeSpan) {
+			makeCloud(t - t__);
+			t__ -= cycleLength;
+		}
     }
 
     /** Make a cloud every <code>cycle</code> period of time. */
     public void tick (float t, float dt) {
-	if (t >= t_) {
-	    makeCloud();
-	    t_ += cycleLength;
-	}
+		if (t >= t_) {
+			makeCloud();
+			t_ += cycleLength;
+		}
     }
 
     private static float STRENGTH_MIN = 0.1f;
 
-    // Makes a new cloud start bubbling up
+    /** Makes a new cloud start bubbling up. */
     private void makeCloud() {
-	if (thermalStrength >= STRENGTH_MIN) {
-	    Cloud cloud = new Cloud(xcModelViewer, x, y, thermalStrength, duration * cycleLength);
-	}
+		if (thermalStrength >= STRENGTH_MIN) {
+			Cloud cloud = new Cloud(xcModelViewer, x, y, thermalStrength, duration * cycleLength);
+		}
     }
 
     /** Makes a cloud that bubbled up at time dt *before* now. */
     private void makeCloud(float dt) {
-	if (thermalStrength >= STRENGTH_MIN) {
-	    Task task = xcModelViewer.xcModel.task;
-	    float x_ = x + dt * task.wind_x;
-	    float y_ = y + dt * task.wind_y;
-	    Cloud cloud = new Cloud(xcModelViewer, x_, y_, thermalStrength, duration * cycleLength);
-	    cloud.setAge(dt);
-	}
+		if (thermalStrength >= STRENGTH_MIN) {
+			Task task = xcModelViewer.xcModel.task;
+			float x_ = x + dt * task.wind_x;
+			float y_ = y + dt * task.wind_y;
+			Cloud cloud = new Cloud(xcModelViewer, x_, y_, thermalStrength, duration * cycleLength);
+			cloud.setAge(dt);
+		}
     }
 
     private float sleepT = -1;
     /**
-       Makes the trigger sleep. When asleep the trigger will not be
-       rendered and does not produce any clouds.
+       Makes the trigger sleep. When asleep the trigger
+       will not be rendered and does not produce any clouds.
      */
     void sleep(float t) {
-	if (mode == SLEEPING) {
-	    return;
-	}
-	xcModelViewer.clock.removeObserver(this);
-	mode = SLEEPING;
-	renderMe();
-	sleepT = t;
+		if (mode == SLEEPING) {
+			return;
+		}
+		xcModelViewer.clock.removeObserver(this);
+		mode = SLEEPING;
+		renderMe();
+		sleepT = t;
     }
 
     /**
@@ -159,18 +160,18 @@ public class Trigger implements ClockObserver, CameraSubject {
        sleep call from one node followed by a wake call from another.
     */
     void wakeUp(float t) {
-	if (mode == AWAKE) {
-	    return;
-	}
-	xcModelViewer.clock.addObserver(this);
-	if (t != sleepT) {
-	    initNextCycle(t);
-	    existingClouds(t);
-	} else {
-	    //System.out.println("Waking up immediately after a sleep");
-	}
-	mode = AWAKE;
-	renderMe();
+		if (mode == AWAKE) {
+			return;
+		}
+		xcModelViewer.clock.addObserver(this);
+		if (t != sleepT) {
+			initNextCycle(t);
+			existingClouds(t);
+		} else {
+			//System.out.println("Waking up immediately after a sleep");
+		}
+		mode = AWAKE;
+		renderMe();
     }
 
     /**
@@ -179,49 +180,48 @@ public class Trigger implements ClockObserver, CameraSubject {
        bubbles occur every <code>cycleLength</code>.
     */
     private void initNextCycle(float t) {
-	t_ = phase;
-	if (t > phase) {
-	    int n = (int) Math.floor((t - phase)/cycleLength);
-	    t_ += (n + 1) * cycleLength;
-	}
+		t_ = phase;
+		if (t > phase) {
+			int n = (int) Math.floor((t - phase)/cycleLength);
+			t_ += (n + 1) * cycleLength;
+		}
     }
 
     public float[] getFocus() {
-	return new float[] {x, y, Cloud.CLOUDBASE/2};
+		return new float[] {x, y, Cloud.CLOUDBASE/2};
     }
 	
     public float[] getEye() {
-	return new float[] {x + Cloud.CLOUDBASE * 2, y, Cloud.CLOUDBASE/2};
+		return new float[] {x + Cloud.CLOUDBASE * 2, y, Cloud.CLOUDBASE/2};
     }
 
     /**
        Add a visual representation of this trigger to the model.
 
-       We draw a square on the ground whose shade varys from white to
-       black as a fn of thermalStrength.
+       We draw a square on the ground whose shade varies from white to
+       black as a function of thermalStrength.
      */
     private Obj3d obj3d;
     private static final int NUM_POINTS = 7;
     private static final float RADIUS = 0.5f;
 
     private void renderMe() {
-	if (mode == AWAKE) {
-	    obj3d = new Obj3d(xcModelViewer, 0, true);
-	    obj3d.setNumPolywires(1);
-	    float radius = thermalStrength * 0.5f;
-	    float[][] ps = Tools3d.circleXY(NUM_POINTS, radius, new float[] {x, y, 0});
-	    obj3d.addPolywireClosed(ps, Obj3d.COLOR_DEFAULT);
-	} else {
-	    obj3d.destroyMe();
-	    obj3d = null;
-	}
+		if (mode == AWAKE) {
+			obj3d = new Obj3d(xcModelViewer, 0, true);
+			obj3d.setNumPolywires(1);
+			float radius = thermalStrength * 0.5f;
+			float[][] ps = Tools3d.circleXY(NUM_POINTS, radius, new float[] {x, y, 0});
+			obj3d.addPolywireClosed(ps, Obj3d.COLOR_DEFAULT);
+		} else {
+			obj3d.destroyMe();
+			obj3d = null;
+		}
     }
 
     /** Prints a debug string. */
     void asString() {
-	System.out.println("Trigger(" + myID + "): x=" + Tools3d.round(x) 
-			   + ", y=" + Tools3d.round(y) + ", mode=" + mode); 
+		System.out.println("Trigger(" + myID + "): x=" + Tools3d.round(x) 
+				   + ", y=" + Tools3d.round(y) + ", mode=" + mode); 
     }
-
-
+	
 }
--- client/TurnPoint.java
+++ client/TurnPoint.java
@@ -12,6 +12,7 @@ package flightclub.client;
 import flightclub.framework3d.*;
 import java.io.*;
 import java.awt.Color;
+
 /**
    This class implements a turn point. Each turn point defines a line
    which runs thru' it and the *next* turn point.
@@ -29,74 +30,70 @@ public class TurnPoint {
     float distanceToNext; // distance to fly to next turn point
     float distanceFromStart;
 
-    /** Changes in x and y as you move a unit of distance along the
-     * line from this turn point to the next. */
+    /*
+		Changes in x and y as you move a unit of distance  
+		along the line from this turn point to the next. 
+	*/
     float dx, dy; 
 
     // unique id for each instance of this class
     static int nextID = 0; 
     int myID;
 
-    /** Define the sector a glider must enter in order to fly *around*
-     * this turn point */
+    // Define the sector a glider must enter in order to fly *around* this turn point
     static final double SECTOR_ANGLE = Math.PI/3;
     static final float SECTOR_DOT = (float) Math.cos(SECTOR_ANGLE);
 
     public TurnPoint(XCModelViewer xcModelViewer, StreamTokenizer st) throws IOException {
-	this.xcModelViewer = xcModelViewer;
-	st.nextToken();
-	x = (float) st.nval;
-	st.nextToken();
-	y = (float) st.nval;
-	st.nextToken(); // gobble end of line
-	myID = nextID++; // unique id (for debugging)
+		this.xcModelViewer = xcModelViewer;
+		st.nextToken();
+		x = (float) st.nval;
+		st.nextToken();
+		y = (float) st.nval;
+		st.nextToken(); // gobble end of line
+		myID = nextID++; // unique id (for debugging)
     }
 
     public TurnPoint(XCModelViewer xcModelViewer, float x, float y) {
-	this.xcModelViewer = xcModelViewer;
-	this.x = x;
-	this.y = y;
-	myID = nextID++; // unique id (for debugging)
+		this.xcModelViewer = xcModelViewer;
+		this.x = x;
+		this.y = y;
+		myID = nextID++; // unique id (for debugging)
     }
 
     /**
-       Sets the next turn point along the course. This allows us to
-       define a line.
+       Sets the next turn point along the course. 
+	   This allows us to define a line.
     */
     void setNextTP(TurnPoint nextTP) {
-	this.nextTP = nextTP;
-	calcLine();
+		this.nextTP = nextTP;
+		calcLine();
     }
 
     /**
-       Sets the previous turn point along the course. This allows us to
-       define the sector for this turn point.
+       Sets the previous turn point along the course. 
+	   This allows us to define the sector for this turn point.
     */
     void setPrevTP(TurnPoint prevTP) {
-	this.prevTP = prevTP;
-	calcSector();
+		this.prevTP = prevTP;
+		calcSector();
     }
 
-    /**
-       Defines the line joining this turn point to the next turn
-       point.
-    */
+    /** Defines the line joining this turn point to the next turn point. */
     private void calcLine() {
-	if (nextTP == null) return;
-	dx = nextTP.x - this.x;
-	dy = nextTP.y - this.y;
+		if (nextTP == null) return;
+		dx = nextTP.x - this.x;
+		dy = nextTP.y - this.y;
 
-	distanceToNext = (float) Math.sqrt(dx * dx + dy * dy);
-	dx /= distanceToNext;
-	dy /= distanceToNext;
+		distanceToNext = (float) Math.sqrt(dx * dx + dy * dy);
+		dx /= distanceToNext;
+		dy /= distanceToNext;
     }
 
-    /**
-       Sets p to be a distance d along line from a to b.
-    */
+    /** Sets p to be a distance d along line from a to b. */
     void getPointOnLine(float d, float[] p) {
-	p[0] = x + d * dx;
-	p[1] = y + d * dy;
+		p[0] = x + d * dx;
+		p[1] = y + d * dy;
     }
 
     private float[] bisect = new float[3];
@@ -107,15 +104,15 @@ public class TurnPoint {
        <code>bisect</code> which defines the sector.
     */
     private void calcSector() {
-	if (prevTP == null) return;
-	bisect[0] = prevTP.dx - this.dx;
-	bisect[1] = prevTP.dy - this.dy;
-	Tools3d.makeUnit(bisect);
-	Tools3d.cross(new float[] {0, 0, 1}, bisect, bisectPerp);
+		if (prevTP == null) return;
+		bisect[0] = prevTP.dx - this.dx;
+		bisect[1] = prevTP.dy - this.dy;
+		Tools3d.makeUnit(bisect);
+		Tools3d.cross(new float[] {0, 0, 1}, bisect, bisectPerp);
 
-	// a point in the sector
-	x_ = x + bisect[0];
-	y_ = y + bisect[1];
+		// a point in the sector
+		x_ = x + bisect[0];
+		y_ = y + bisect[1];
     }
 
     /**
@@ -126,24 +123,24 @@ public class TurnPoint {
        Todo: debug
     */
     boolean inSector(float[] p) {
-	float[] r = new float[3];
-	Tools3d.subtract(p, new float[] {x, y, p[2]}, r);
-	Tools3d.makeUnit(r);
-	if (nextTP != null) {
-	    return Tools3d.dot(r, bisect) >= SECTOR_DOT;
-	} else {
-	    // finish line - cos 90 degrees is zero
-	    return Tools3d.dot(r, bisect) >= 0;
-	}
+		float[] r = new float[3];
+		Tools3d.subtract(p, new float[] {x, y, p[2]}, r);
+		Tools3d.makeUnit(r);
+		if (nextTP != null) {
+			return Tools3d.dot(r, bisect) >= SECTOR_DOT;
+		} else {
+			// finish line - cos 90 degrees is zero
+			return Tools3d.dot(r, bisect) >= 0;
+		}
     }
 
     /** Prints debug info. */
     void asString() {
-	System.out.println("TurnPoint(" + myID 
-			   + "): x=" + Tools3d.round(x) + ", y=" + Tools3d.round(y) 
-			   + ", dx=" + Tools3d.round(dx) + ", dy=" + Tools3d.round(dy) 
-			   + ", d1=" + distanceFromStart
-			   + ", d2=" + distanceToNext);
+		System.out.println("TurnPoint(" + myID 
+				   + "): x=" + Tools3d.round(x) + ", y=" + Tools3d.round(y) 
+				   + ", dx=" + Tools3d.round(dx) + ", dy=" + Tools3d.round(dy) 
+				   + ", d1=" + distanceFromStart
+				   + ", d2=" + distanceToNext);
     }
 
     /**
@@ -154,15 +151,15 @@ public class TurnPoint {
        B. 
      */
     void renderMe() {
-	if (prevTP == null) {
-	    renderStart();
-	    renderArrows();
-	} else if (nextTP != null) {
-	    renderSector();
-	    renderArrows();
-	} else {
-	    renderFinish();
-	}
+		if (prevTP == null) {
+			renderStart();
+			renderArrows();
+		} else if (nextTP != null) {
+			renderSector();
+			renderArrows();
+		} else {
+			renderFinish();
+		}
     }
 
     /** Sector has radius and perpendicular width. */
@@ -185,32 +182,32 @@ public class TurnPoint {
        </pre>
     */
     private void renderSector() {
-	if (!xcModelViewer.xcModel.task.nodeManager.contains(x, y)){
-	    if (objA != null) {
-		objA.destroyMe();
-		objA = null;
-	    }
-	    return;
-	}
+		if (!xcModelViewer.xcModel.task.nodeManager.contains(x, y)) {
+			if (objA != null) {
+				objA.destroyMe();
+				objA = null;
+			}
+			return;
+		}
 
-	// have we already created the sector ?
-	if (objA != null) {
-	    return;
-	}
+		// have we already created the sector ?
+		if (objA != null) {
+			return;
+		}
 
-	float[][] ps = new float[3][3];
-	float [] p = new float[3];
-	Tools3d.linearSum(radius, bisect, -dPerp, bisectPerp, ps[1]);
-	Tools3d.linearSum(radius, bisect, dPerp, bisectPerp, ps[2]);
+		float[][] ps = new float[3][3];
+		float [] p = new float[3];
+		Tools3d.linearSum(radius, bisect, -dPerp, bisectPerp, ps[1]);
+		Tools3d.linearSum(radius, bisect, dPerp, bisectPerp, ps[2]);
 
-	for (int i = 0; i < 3; i++) {
-	    ps[i][0] += x;
-	    ps[i][1] += y;
-	}
+		for (int i = 0; i < 3; i++) {
+			ps[i][0] += x;
+			ps[i][1] += y;
+		}
 
-	objA = new Obj3d(xcModelViewer, 0, true);
-	objA.setNumPolywires(1);
-	objA.addPolywireClosed(ps, COLOR_SECTOR);
+		objA = new Obj3d(xcModelViewer, 0, true);
+		objA.setNumPolywires(1);
+		objA.addPolywireClosed(ps, COLOR_SECTOR);
     }
 
     private Obj3d objB = null; // arrows
@@ -233,109 +230,109 @@ public class TurnPoint {
        </pre>
     */
     private void renderArrows() {
-	float[] a = new float[] {x, y, 0};
-	float[] b = new float[]{a[0] + ARROW_LEN * dx, a[1] + ARROW_LEN * dy, 0};
-	float[] c = new float[]{a[0] + (ARROW_LEN - ARROW_HEAD) * dx, 
-				a[1] + (ARROW_LEN - ARROW_HEAD) * dy, 0};
-	float[] d = new float[]{c[0], c[1], c[2]};
-	c[0] += dy * ARROW_HEAD;
-	c[1] += - dx * ARROW_HEAD;
-	d[0] += - dy * ARROW_HEAD;
-	d[1] +=  dx * ARROW_HEAD;
+		float[] a = new float[] {x, y, 0};
+		float[] b = new float[]{a[0] + ARROW_LEN * dx, a[1] + ARROW_LEN * dy, 0};
+		float[] c = new float[]{a[0] + (ARROW_LEN - ARROW_HEAD) * dx, 
+					a[1] + (ARROW_LEN - ARROW_HEAD) * dy, 0};
+		float[] d = new float[]{c[0], c[1], c[2]};
+		c[0] += dy * ARROW_HEAD;
+		c[1] += - dx * ARROW_HEAD;
+		d[0] += - dy * ARROW_HEAD;
+		d[1] +=  dx * ARROW_HEAD;
 
-	float dx_ = dx * ARROW_SPACING;
-	float dy_ = dy * ARROW_SPACING;
-	float[][][] pss = new float[ARROWS_MAX][2][3]; // tails
-	float[][][] qss = new float[ARROWS_MAX][3][3]; // heads
-	NodeManager nodeManager = xcModelViewer.xcModel.task.nodeManager;
+		float dx_ = dx * ARROW_SPACING;
+		float dy_ = dy * ARROW_SPACING;
+		float[][][] pss = new float[ARROWS_MAX][2][3]; // tails
+		float[][][] qss = new float[ARROWS_MAX][3][3]; // heads
+		NodeManager nodeManager = xcModelViewer.xcModel.task.nodeManager;
 
-	// start afresh
-	if (objB != null) {
-	    objB.destroyMe();
-	}
-
-	int n = 0;
-	float dist = 0;
-
-	while (dist < distanceToNext) {
-	    if (nodeManager.contains(a[0], a[1])) {
-		pss[n][0][0] = a[0];
-		pss[n][0][1] = a[1];
-		pss[n][1][0] = b[0];
-		pss[n][1][1] = b[1];
-
-		qss[n][0][0] = c[0];
-		qss[n][0][1] = c[1];
-		qss[n][1][0] = b[0];
-		qss[n][1][1] = b[1];
-		qss[n][2][0] = d[0];
-		qss[n][2][1] = d[1];
-
-		n++;
-		if (n > ARROWS_MAX) { // ideally, should not hit this
-		    break;
+		// start afresh
+		if (objB != null) {
+			objB.destroyMe();
 		}
-	    }
-	    dist += ARROW_SPACING;
-	    a[0] += dx_;
-	    a[1] += dy_;
-	    b[0] += dx_;
-	    b[1] += dy_;
-	    c[0] += dx_;
-	    c[1] += dy_;
-	    d[0] += dx_;
-	    d[1] += dy_;
-	}
 
-	objB = new Obj3d(xcModelViewer, 0, true);
-	objB.setNumPolywires(n * 2); // each arrow has two wires (head and tail)
+		int n = 0;
+		float dist = 0;
 
-	for (int i = 0; i < n; i++) { // note we use n and *not* ps.length
-	    objB.addPolywire(pss[i], COLOR_ARROW);
-	    objB.addPolywire(qss[i], COLOR_ARROW);
-	}
+		while (dist < distanceToNext) {
+			if (nodeManager.contains(a[0], a[1])) {
+				pss[n][0][0] = a[0];
+				pss[n][0][1] = a[1];
+				pss[n][1][0] = b[0];
+				pss[n][1][1] = b[1];
+
+				qss[n][0][0] = c[0];
+				qss[n][0][1] = c[1];
+				qss[n][1][0] = b[0];
+				qss[n][1][1] = b[1];
+				qss[n][2][0] = d[0];
+				qss[n][2][1] = d[1];
+
+				n++;
+				if (n > ARROWS_MAX) { // ideally, should not hit this
+					break;
+				}
+			}
+			dist += ARROW_SPACING;
+			a[0] += dx_;
+			a[1] += dy_;
+			b[0] += dx_;
+			b[1] += dy_;
+			c[0] += dx_;
+			c[1] += dy_;
+			d[0] += dx_;
+			d[1] += dy_;
+		}
+
+		objB = new Obj3d(xcModelViewer, 0, true);
+		objB.setNumPolywires(n * 2); // each arrow has two wires (head and tail)
+
+		for (int i = 0; i < n; i++) { // note we use n and *not* ps.length
+			objB.addPolywire(pss[i], COLOR_ARROW);
+			objB.addPolywire(qss[i], COLOR_ARROW);
+		}
     }
 
     /** Draws a perp line. */
     private void renderStart() {
-	if (!xcModelViewer.xcModel.task.nodeManager.contains(x, y)){
-	    if (objA != null) {
-		objA.destroyMe();
-		objA = null;
-	    }
-	    return;
-	}
+		if (!xcModelViewer.xcModel.task.nodeManager.contains(x, y)){
+			if (objA != null) {
+				objA.destroyMe();
+				objA = null;
+			}
+			return;
+		}
 
-	float[][] ps = new float[2][3];
-	ps[0][0] = x - dPerp * dy;
-	ps[0][1] = y + dPerp * dx;
-	ps[1][0] = x + dPerp * dy;
-	ps[1][1] = y - dPerp * dx;
+		float[][] ps = new float[2][3];
+		ps[0][0] = x - dPerp * dy;
+		ps[0][1] = y + dPerp * dx;
+		ps[1][0] = x + dPerp * dy;
+		ps[1][1] = y - dPerp * dx;
 
-	objA = new Obj3d(xcModelViewer, 0, true);
-	objA.setNumPolywires(1);
-	objA.addPolywireClosed(ps, COLOR_SECTOR);
+		objA = new Obj3d(xcModelViewer, 0, true);
+		objA.setNumPolywires(1);
+		objA.addPolywireClosed(ps, COLOR_SECTOR);
     }
 
     // for now...
     private void renderFinish() { 
-	if (!xcModelViewer.xcModel.task.nodeManager.contains(x, y)){
-	    if (objA != null) {
-		objA.destroyMe();
-		objA = null;
-	    }
-	    return;
-	}
+		if (!xcModelViewer.xcModel.task.nodeManager.contains(x, y)){
+			if (objA != null) {
+				objA.destroyMe();
+				objA = null;
+			}
+			return;
+		}
 
-	float[][] ps = new float[2][3];
-	ps[0][0] = x - dPerp * prevTP.dy;
-	ps[0][1] = y + dPerp * prevTP.dx;
-	ps[1][0] = x + dPerp * prevTP.dy;
-	ps[1][1] = y - dPerp * prevTP.dx;
+		float[][] ps = new float[2][3];
+		ps[0][0] = x - dPerp * prevTP.dy;
+		ps[0][1] = y + dPerp * prevTP.dx;
+		ps[1][0] = x + dPerp * prevTP.dy;
+		ps[1][1] = y - dPerp * prevTP.dx;
 
-	objA = new Obj3d(xcModelViewer, 0, true);
-	objA.setNumPolywires(1);
-	objA.addPolywireClosed(ps, COLOR_SECTOR);
+		objA = new Obj3d(xcModelViewer, 0, true);
+		objA.setNumPolywires(1);
+		objA.addPolywireClosed(ps, COLOR_SECTOR);
     }
 
 }
