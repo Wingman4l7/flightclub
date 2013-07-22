d4d1fca57422c9661b6f083b189e917858e86b0b
W.M. <Wingman4l7@gmail.com>
2013-07-23 09:41:13 +1000

    normalized indentation

--- client/GliderAI.java
+++ client/GliderAI.java
@@ -12,12 +12,13 @@ package flightclub.client;
 import flightclub.framework3d.*;
 import java.awt.*;
 import java.util.*;
+
 /**
   This class implements an AI glider. The glider will sniff out lift,
   climb to base then fly downwind in search of more lift.
 
-  TODO: When to climb and when to glide to a *better* climb ? Also, a
-  rule that if h < h1 then be *cautious* ?
+  TODO: When to climb and when to glide to a *better* climb?
+  Also, a rule that if h < h1 then be *cautious*?
   
   TODO: Different AI profiles: eg. racer vs floater
 */
@@ -38,224 +39,213 @@ public class GliderAI extends GliderTask {
     //static final Color[] colors = {Color.PINK, Color.ORANGE, Color.GREEN, Color.RED, Color.BLUE};
 
     public GliderAI(XCModelViewer xcModelViewer, GliderType gliderType, int id) { 
-	super(xcModelViewer, gliderType, id);
-	moveManager = new MovementManager(xcModelViewer, this);
-	//this.obj.setColor(colors[myID%colors.length]);
+		super(xcModelViewer, gliderType, id);
+		moveManager = new MovementManager(xcModelViewer, this);
+		//this.obj.setColor(colors[myID%colors.length]);
     }
 
-    /**
-       Start flying the task.
-     */
+    /** Start flying the task. */
     public void takeOff() {
-	super.takeOff();
-	makeDecision(xcModelViewer.clock.getTime());
+		super.takeOff();
+		makeDecision(xcModelViewer.clock.getTime());
     }
 
     // time of last search for lift (an expensive operation)
     private float t_ = 0;
 
-    /**
-       Searches my node for the next lift source.
-    */
+    /** Searches my node for the next lift source. */
     void makeDecision(float t) {
-	Node[] nodes = xcModelViewer.xcModel.task.nodeManager.loadedNodes();
-	LiftSource ls = null;
-	for (int i = 0; i < nodes.length; i++) {
-	    ls = nodes[i].search(this);
-	    if (ls != null) {
-		break;
-	    }
-	}
-	t_ = t; // note time of search
+		Node[] nodes = xcModelViewer.xcModel.task.nodeManager.loadedNodes();
+		LiftSource ls = null;
+		for (int i = 0; i < nodes.length; i++) {
+			ls = nodes[i].search(this);
+			if (ls != null) {
+				break;
+			}
+		}
+		t_ = t; // note time of search
 
-	if (ls == null) { // glide torwards next turn point
-	    this.moveManager.setTargetPoint(new float[] {nextTP.x_, nextTP.y_, 0}, true);
-	    tryLater = true;
-	    return;
-	}
+		if (ls == null) { // glide torwards next turn point
+			this.moveManager.setTargetPoint(new float[] {nextTP.x_, nextTP.y_, 0}, true);
+			tryLater = true;
+			return;
+		}
 
-	// glide to lift source - a cloud or hill
-	try {
-	    Cloud cloud = (Cloud) ls;
-	    this.moveManager.setCloud(cloud);
-	    if (filmID == myID) {
-		cutPending = true;
-		cutWhen = t + whenArrive(cloud.x, cloud.y) - 2; 
-		cutSubject = (CameraSubject) cloud;
-	    }
-	} catch (Exception e) {;}
+		// glide to lift source - a cloud or hill
+		try {
+			Cloud cloud = (Cloud) ls;
+			this.moveManager.setCloud(cloud);
+			if (filmID == myID) {
+				cutPending = true;
+				cutWhen = t + whenArrive(cloud.x, cloud.y) - 2; 
+				cutSubject = (CameraSubject) cloud;
+			}
+		} catch (Exception e) {;}
 
-	try {
-	    Hill hill = (Hill) ls;
-	    this.moveManager.setCircuit(hill.getCircuit());
-	} catch (Exception e) {;}
+		try {
+			Hill hill = (Hill) ls;
+			this.moveManager.setCircuit(hill.getCircuit());
+		} catch (Exception e) {;}
     }	
 
-    /**
-       Is there a better lift source within reach ?
-    */
+    /** Is there a better lift source within reach? */
     void reviewDecision(float t) {
-	Node[] nodes = xcModelViewer.xcModel.task.nodeManager.loadedNodes();
-	LiftSource ls = null;
-	for (int i = 0; i < nodes.length; i++) {
-	    ls = nodes[i].search(this);
-	    if (ls != null) {
-		break;
-	    }
-	}
-	t_ = t; // note time of search
-
-	if (ls == null) {
-	    // stick to last decision
-	    return;
-	}
-
-	// glide to cloud if it is *stronger*
-	try {
-	    Cloud cloud = (Cloud) ls;
-	    if (moveManager.cloud == null || cloud.getLift() > moveManager.cloud.getLift()) {
-		this.moveManager.setCloud(cloud);
-		if (filmID == myID) {
-		    cutPending = true;
-		    cutWhen = t + whenArrive(cloud.x, cloud.y) - 2;
-		    cutSubject = (CameraSubject) cloud;
+		Node[] nodes = xcModelViewer.xcModel.task.nodeManager.loadedNodes();
+		LiftSource ls = null;
+		for (int i = 0; i < nodes.length; i++) {
+			ls = nodes[i].search(this);
+			if (ls != null) {
+				break;
+			}
 		}
-	    }
-	} catch (Exception e) {;}
+		t_ = t; // note time of search
 
-	try {
-	    Hill hill = (Hill) ls;
-	    this.moveManager.setCircuit(hill.getCircuit());
-	} catch (Exception e) {;}
+		if (ls == null) {
+			// stick to last decision
+			return;
+		}
+
+		// glide to cloud if it is *stronger*
+		try {
+			Cloud cloud = (Cloud) ls;
+			if (moveManager.cloud == null || cloud.getLift() > moveManager.cloud.getLift()) {
+				this.moveManager.setCloud(cloud);
+				if (filmID == myID) {
+					cutPending = true;
+					cutWhen = t + whenArrive(cloud.x, cloud.y) - 2;
+					cutSubject = (CameraSubject) cloud;
+				}
+			}
+		} catch (Exception e) {;}
+
+		try {
+			Hill hill = (Hill) ls;
+			this.moveManager.setCircuit(hill.getCircuit());
+		} catch (Exception e) {;}
     }	
 
     public void tick(float t, float dt) {
-	if (!landed) {
-	    nextTurn = moveManager.nextMove();
-	    tickAI(t);
-	}
-	super.tick(t, dt);
+		if (!landed) {
+			nextTurn = moveManager.nextMove();
+			tickAI(t);
+		}
+		super.tick(t, dt);
     }
 
     private boolean finalGlide = false; // set to true when final gliding
     static final float T_THINK = 1.0f; // don't think too hard (CPU)
     /**
-       Checks for certain conditions. Eg. have i reached base ? 
+       Checks for certain conditions. Eg. have I reached base? 
 
-       TODO: A better design would not keep checking every tick, but
-       use a call back ?
+       TODO: A better design would not keep checking every tick, 
+	   but use a call back?
     */
     protected void tickAI(float t) {
-	// time for a camera move ?
-        if (cutPending) {
-            if (t >= cutWhen) {
-		cutNow();
-	    }
-        }
+		// time for a camera move ?
+			if (cutPending) {
+				if (t >= cutWhen) {
+					cutNow();
+				}
+			}
 
-	if (finalGlide) {
-	    return;
-	}
+		if (finalGlide) {
+			return;
+		}
 
-	// final glide
-	if (nextTP.nextTP == null) {
-	    if (withinGlide(nextTP.x, nextTP.y)) {
-		this.moveManager.setTargetPoint(new float[] {nextTP.x_, nextTP.y_, 0}, true);
-		finalGlide = true;
-	    }
-	}
+		// final glide
+		if (nextTP.nextTP == null) {
+			if (withinGlide(nextTP.x, nextTP.y)) {
+				this.moveManager.setTargetPoint(new float[] {nextTP.x_, nextTP.y_, 0}, true);
+				finalGlide = true;
+			}
+		}
 
-	// am i thermalling under a decaying cloud ?
-	if (moveManager.cloud != null && moveManager.cloud.lifeCycle.isDecaying()) {
-	    if (Glider.filmID == myID) {
-		modelViewer.cameraMan.setSubject(this, true);
-	    }
-	    makeDecision(t);
-	    return;
-	}
+		// am I thermalling under a decaying cloud?
+		if (moveManager.cloud != null && moveManager.cloud.lifeCycle.isDecaying()) {
+			if (Glider.filmID == myID) {
+				modelViewer.cameraMan.setSubject(this, true);
+			}
+			makeDecision(t);
+			return;
+		}
 
-	// am i at base ?
-	if (this.obj.getZmax() >= Cloud.CLOUDBASE && t > t_ + T_LATER) {
-	    if (Glider.filmID == myID) {
-		modelViewer.cameraMan.setSubject(this, true);
-	    }
-	    makeDecision(t);
-	    return;
-	}
+		// am I at base?
+		if (this.obj.getZmax() >= Cloud.CLOUDBASE && t > t_ + T_LATER) {
+			if (Glider.filmID == myID) {
+				modelViewer.cameraMan.setSubject(this, true);
+			}
+			makeDecision(t);
+			return;
+		}
 
-	// am i gliding and waiting 
-	if (tryLater) {
-	    if (t > t_ + T_LATER) {
-		tryLater = false;
-		makeDecision(t);
-	    }
-	    return;
-	}
+		// am I gliding and waiting? 
+		if (tryLater) {
+			if (t > t_ + T_LATER) {
+				tryLater = false;
+				makeDecision(t);
+			}
+			return;
+		}
 
-	// otherwise, review me last decision every so often - perhaps
-	// we can now reach a better climb ?
-	if (t > t_ + T_THINK) {
-	    reviewDecision(t);	    
-	}
+		// otherwise, review me last decision every so often - 
+		// perhaps we can now reach a better climb?
+		if (t > t_ + T_THINK) {
+			reviewDecision(t);	    
+		}
     }
 
 
     protected void reachedTurnPoint() {
-	makeDecision(xcModelViewer.clock.getTime());
+		makeDecision(xcModelViewer.clock.getTime());
     }
 
     /**
        Returns true if (x, y) is within glide. Adding wind makes this
-       a bit fiddly. The glide angle will be reduced by any head wind
-       etc.
+       a bit fiddly. The glide angle will be reduced by any head wind etc.
     */
     private boolean withinGlide(float x, float y) {
-	float[] u = this.onTrack(); 
-	float s = this.getSpeed();
-	u[0] *= s;
-	u[1] *= s;
-	u[0] += air[0];
-	u[1] += air[1];
-	float s_ = (float) Math.sqrt(u[0] * u[0] + u[1] * u[1]);
-	float glideAngle_ = s_/- this.getSink();
+		float[] u = this.onTrack(); 
+		float s = this.getSpeed();
+		u[0] *= s;
+		u[1] *= s;
+		u[0] += air[0];
+		u[1] += air[1];
+		float s_ = (float) Math.sqrt(u[0] * u[0] + u[1] * u[1]);
+		float glideAngle_ = s_/- this.getSink();
 
-	float[] r = new float[3];
-	Tools3d.subtract(new float[] {x, y, p[2]}, p, r);
-	float d = Tools3d.length(r);
-	return d <= p[2] * glideAngle_;
+		float[] r = new float[3];
+		Tools3d.subtract(new float[] {x, y, p[2]}, p, r);
+		float d = Tools3d.length(r);
+		return d <= p[2] * glideAngle_;
     }
-
-
 
     /**
        Returns a unit vector pointing in the direction we want to
        fly. Note we glide to (x_, y_), a point inside the turn point
        sector rather than (x, y) itself.
 
-       Todo: take into account the wind exactly (need to solve a
-       quadratic eq). I have an approx solution which is ok for light
-       winds.
+       TODO: take into account the wind exactly (need to solve a quadratic eq). 
+	   I have an approx solution which is ok for light winds.
     */
     float[] onTrack() {
-	float[] r = new float[3];
-	Tools3d.subtract(new float[] {nextTP.x_, nextTP.y_, p[2]}, p, r);
+		float[] r = new float[3];
+		Tools3d.subtract(new float[] {nextTP.x_, nextTP.y_, p[2]}, p, r);
 
-	// time to get to next turn point assuming nil wind
-	float t = Tools3d.length(r)/this.getSpeed();
+		// time to get to next turn point assuming nil wind
+		float t = Tools3d.length(r)/this.getSpeed();
 
-	// drift due to wind during this amount of time
-	float[] drift = new float[] {air[0] * t, air[1] * t, 0};
+		// drift due to wind during this amount of time
+		float[] drift = new float[] {air[0] * t, air[1] * t, 0};
 
-	// offset turn point by - drift to get ~desired heading
-	Tools3d.subtract(new float[] {nextTP.x_ - drift[0], nextTP.y_ - drift[1], p[2]}, p, r);
-	Tools3d.makeUnit(r);
-	return r;
+		// offset turn point by - drift to get ~desired heading
+		Tools3d.subtract(new float[] {nextTP.x_ - drift[0], nextTP.y_ - drift[1], p[2]}, p, r);
+		Tools3d.makeUnit(r);
+		return r;
     }
 
-
     /**
-       How long to get to a cloud. Note we may ignore the wind as both
-       the cloud and the glider experience the same drift.
+       How long to get to a cloud. Note we may ignore the wind as 
+       both the cloud and the glider experience the same drift.
     */
     float whenArrive(float x, float y) {
         float d = (p[0] - x) * (p[0] - x) + (p[1] - y) * (p[1] - y);
@@ -269,10 +259,9 @@ public class GliderAI extends GliderTask {
        ahead of its subject.
     */
     private void cutNow() {
-	if (Glider.filmID == myID) {
-	    modelViewer.cameraMan.setSubject(cutSubject, true);
-	}
-        
+		if (Glider.filmID == myID) {
+			modelViewer.cameraMan.setSubject(cutSubject, true);
+		}
         cutPending = false;
         cutSubject = null;
     }
--- client/GliderUser.java
+++ client/GliderUser.java
@@ -13,36 +13,34 @@ import flightclub.framework3d.*;
 import java.awt.*;
 import java.awt.event.*;
 
-/**
-   This class implements a glider that is controlled by *the* user.  
-*/
+/** This class implements a glider that is controlled by *the* user.  */
 public class GliderUser extends GliderTask implements EventInterface {
     Variometer vario;
     int cameraMode = XCCameraMan.USER;
 
     public GliderUser(XCModelViewer xcModelViewer, GliderType gliderType, int id) { 
-	super(xcModelViewer, gliderType, id);
-	modelViewer.eventManager.addNotification(this); // i want to hear about keyboard events
-	//this.obj.setColor(new Color(255, 200, 0)); // orange
-	vario = new Variometer(xcModelViewer, this);
+		super(xcModelViewer, gliderType, id);
+		modelViewer.eventManager.addNotification(this); // I want to hear about keyboard events
+		//this.obj.setColor(new Color(255, 200, 0)); // orange
+		vario = new Variometer(xcModelViewer, this);
     }
 
     protected void createTail() {
-	tail = new Tail(modelViewer, this, new Color(255, 200, 0), Tail.NUM_WIRES * 2, Tail.WIRE_EVERY);
-	tail.init();
+		tail = new Tail(modelViewer, this, new Color(255, 200, 0), Tail.NUM_WIRES * 2, Tail.WIRE_EVERY);
+		tail.init();
     }
 
     public void destroyMe() {
-	super.destroyMe();
-	modelViewer.eventManager.removeNotification(this);
+		super.destroyMe();
+		modelViewer.eventManager.removeNotification(this);
     }
 
     public void tick(float t, float dt) {
-	super.tick(t, dt);
-	if (!landed) {
-	    vario.tick(t);
-	    netSend();
-	}
+		super.tick(t, dt);
+		if (!landed) {
+			vario.tick(t);
+			netSend();
+		}
     }
 
     static final int PG_STYLE = 0;
@@ -64,63 +62,63 @@ public class GliderUser extends GliderTask implements EventInterface {
        control ?
     */
     public void keyPressed(KeyEvent e) {
-	if (landed) {
-	    return;
-	}
+		if (landed) {
+			return;
+		}
 
-	int key = e.getKeyCode();
-        switch (key) {
-        case 37://left arrow
-	    if (controlStyle == PG_STYLE) {
-		setMove(-1);
-	    } else {
-		incMove(-1);
-	    }
-	    if (((XCCameraMan)modelViewer.cameraMan).mode == XCCameraMan.USER) {
-		modelViewer.cameraMan.setSubject(this, true);
-	    }
-            break;
-        case 39://right arrow
-	    if (controlStyle == PG_STYLE) {
-		setMove(1);
-	    } else {
-		incMove(1);
-	    }
-	    if (((XCCameraMan)modelViewer.cameraMan).mode == XCCameraMan.USER) {
-		modelViewer.cameraMan.setSubject(this, true);
-	    }
-            break;
-        case 32://space
-	    setPolar(0); // slowest
-	    setMove(-1); // circle left
-            break;
-        case 38://up arrow
-        case 38-32:
-	    setMove(0); // onwards
-            goFaster();
-            break;
-        case 40://down arrow
-        case 40-32:
-	    setMove(0); // onwards
-            goSlower();
-            break;
-        default:
-        }
+		int key = e.getKeyCode();
+			switch (key) {
+			case 37: // left arrow
+				if (controlStyle == PG_STYLE) {
+					setMove(-1);
+				} else {
+					incMove(-1);
+				}
+				if (((XCCameraMan)modelViewer.cameraMan).mode == XCCameraMan.USER) {
+					modelViewer.cameraMan.setSubject(this, true);
+				}
+			break;
+			case 39: // right arrow
+				if (controlStyle == PG_STYLE) {
+					setMove(1);
+				} else {
+					incMove(1);
+				}
+				if (((XCCameraMan)modelViewer.cameraMan).mode == XCCameraMan.USER) {
+					modelViewer.cameraMan.setSubject(this, true);
+				}
+			break;
+			case 32: // space
+				setPolar(0); // slowest
+				setMove(-1); // circle left
+			break;
+			case 38: // up arrow
+			case 38-32:
+				setMove(0); // onwards
+				goFaster();
+			break;
+			case 40: // down arrow
+			case 40-32:
+				setMove(0); // onwards
+				goSlower();
+			break;
+			default:
+			}
     }
 
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-        case 37://left arrow
-	    if (controlStyle == PG_STYLE) {
-		setMove(0);
-	    }
-            break;
-        case 39://right arrow
-	    if (controlStyle == PG_STYLE) {
-		setMove(0);
-	    }
-            break;
+        case 37: // left arrow
+			if (controlStyle == PG_STYLE) {
+				setMove(0);
+			}
+        break;
+        case 39: // right arrow
+			if (controlStyle == PG_STYLE) {
+				setMove(0);
+			}
+        break;
         default:
         }
     }
@@ -128,78 +126,78 @@ public class GliderUser extends GliderTask implements EventInterface {
     private void setMove(int move) { nextTurn = move; }
 
     private void incMove(int move) {
-	nextTurn += move; 
-	if (nextTurn > 1) nextTurn = 1;
-	if (nextTurn < -1) nextTurn = -1;
+		nextTurn += move; 
+		if (nextTurn > 1) nextTurn = 1;
+		if (nextTurn < -1) nextTurn = -1;
     }
-
 
     private int _nextTurn = 0;
     private int _iP = 0;
     /**
-       Send to server the values of p, vx, vy, iP and the next
-       move. We only do this if either nextTurn has changed or iP has
-       changed since this fn was last called.
+       Send to server the values of p, vx, vy, iP and the next move.
+       We only do this if either nextTurn has changed or iP has
+       changed since this function was last called.
     */
     private void netSend() { 
-	if (nextTurn != _nextTurn || iP != _iP) {
-	    if (xcModelViewer.xcNet != null) {
-		xcModelViewer.xcNet.send("#" + 
-					     round(this.p[0]) + ":" + 
-					     round(this.p[1]) + ":" + 
-					     round(this.p[2]) + ":" + 
-					     round(this.v[0]) + ":" + 
-					     round(this.v[1]) + ":" +
-					     this.iP + ":" + 
-					     (int) nextTurn);
-		_nextTurn = (int) nextTurn;
-		_iP = iP;
-	    }
-	}
+		if (nextTurn != _nextTurn || iP != _iP) {
+			if (xcModelViewer.xcNet != null) {
+				xcModelViewer.xcNet.send("#" + 
+								 round(this.p[0]) + ":" + 
+								 round(this.p[1]) + ":" + 
+								 round(this.p[2]) + ":" + 
+								 round(this.v[0]) + ":" + 
+								 round(this.v[1]) + ":" +
+								 this.iP + ":" + 
+								 (int) nextTurn);
+				_nextTurn = (int) nextTurn;
+				_iP = iP;
+			}
+		}
     }
 
     /** Round to floats to n decimal places. */
     private float round(float x) {
-	return Math.round(x * 10000f)/10000f;
+		return Math.round(x * 10000f)/10000f;
     }
 
     void hitTheSpuds() {
-	super.hitTheSpuds();
-	if (xcModelViewer.xcNet != null) {
-	    xcModelViewer.xcNet.send("Landed");
-	}
+		super.hitTheSpuds();
+		if (xcModelViewer.xcNet != null) {
+			xcModelViewer.xcNet.send("Landed");
+		}
     }
 
     public void takeOff() {
-	super.takeOff();
-	if (xcModelViewer.xcNet != null) {
-	    xcModelViewer.xcNet.send("Launched: " + this.typeID);
-	}
+		super.takeOff();
+		if (xcModelViewer.xcNet != null) {
+			xcModelViewer.xcNet.send("Launched: " + this.typeID);
+		}
     }
 
     public float[] getFocus() {
-	if (cameraMode == XCCameraMan.USER) {
-	    return super.getFocus();
-	} else {
-	    TurnPoint tp = nextTP.prevTP;
-	    return new float[] {p[0] + v[0] * EYE_D, p[1] + v[1] * EYE_D, p[2] + v[2] * EYE_D}; 
-	}
+		if (cameraMode == XCCameraMan.USER) {
+			return super.getFocus();
+		} else {
+			TurnPoint tp = nextTP.prevTP;
+			return new float[] {p[0] + v[0] * EYE_D, p[1] + v[1] * EYE_D, p[2] + v[2] * EYE_D}; 
+		}
     }
 
     private final float EYE_DD = 0.1f;
-    
     public float[] getEye() {
-	if (cameraMode == XCCameraMan.USER) {
-	    return super.getEye();
-	} else {
-	    TurnPoint tp = nextTP.prevTP;
-	    return new float[] {p[0] + v[0] * EYE_DD, p[1] + v[1] * EYE_DD, p[2] + v[2] * EYE_DD}; 
-	}
+		if (cameraMode == XCCameraMan.USER) {
+			return super.getEye();
+		} else {
+			TurnPoint tp = nextTP.prevTP;
+			return new float[] {p[0] + v[0] * EYE_DD, p[1] + v[1] * EYE_DD, p[2] + v[2] * EYE_DD}; 
+		}
     }
 
     /**
-       Allows camera man to either follow the glider from behind or look
-       from the point of view of the pilot.
+       Allows camera man to either follow the glider from behind
+	   or look from the point of view of the pilot.
     */
-    void setCameraMode(int mode) { cameraMode = mode; }
+    void setCameraMode(int mode) {
+		cameraMode = mode; 
+	}
 }
