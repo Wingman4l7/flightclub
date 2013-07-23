5cb6c607f7c82de9a393223094eddf14ab7f0404
W.M <Wingman4l7@gmail.com>
2013-07-23 10:37:50 +1000

    Revert "added missing license header"
    This reverts commit 6449a4e8c5a1ed49382289a0f549cd6e067787d6.

--- framework3d/FileFormatException.java
+++ framework3d/FileFormatException.java
@@ -1,17 +1,9 @@
-/*
- * @(#)CameraSubject.java (part of 'Flight Club')
- * 
- * This code is covered by the GNU General Public License
- * detailed at http://www.gnu.org/copyleft/gpl.html
- *	
- * Flight Club docs located at http://www.danb.dircon.co.uk/hg/hg.htm
- * Copyright 2001-2002 Dan Burton <danb@dircon.co.uk>
- */
+/** This exception may be thrown by parsers. */
+
 package flightclub.framework3d;
 
 import java.io.*;
 
-/** This exception may be thrown by parsers. */
 public class FileFormatException extends IOException {
     public FileFormatException(String s) { super(s); }
 }
